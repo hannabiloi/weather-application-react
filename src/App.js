d5479243e8ace1072ada45923e0b496afc83998a
hanna biloi <beloy1991@gmail.com>
2022-08-17 13:38:53 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Hanna Biloi and is{" "}
+        <a
+          href="httfooters://github.com/hannabiloi/weather-application-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
