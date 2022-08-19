b8f15e5faf4ff13f0830fe326a51a458c2b61d83
hanna biloi <beloy1991@gmail.com>
2022-08-19 16:31:48 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather defaultCity="Kyiv"/>
         <footer>
           This project was coded by Hanna Biloi and is{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import "./Weather.css";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      tempMax: response.data.main.temp_max,
+      tempMin: response.data.main.temp_min,
+      wind: response.data.wind.speed,
+      sky: response.data.weather[0].main,
+      icon: `./images/${response.data.weather[0].icon}.svg`,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <h1>{weatherData.city}</h1>
+        <h2>19/08/2022</h2>
+        <div className="row justify-content-md-center current-weather align-items-center">
+          <div className="col-4">
+            <span>{Math.round(weatherData.temperature)}</span>
+            <a href="#" className="active" id="celsius">
+              °C
+            </a>
+            |
+            <a href="#" id="fahrenheit">
+              °F
+            </a>
+          </div>
+          <div className="col-4">
+            <img
+              src={weatherData.icon}
+              alt=""
+              className="main-icon"
+              id="icon"
+            />
+          </div>
+          <div className="col-4" id="weather-sky">
+            {weatherData.sky}
+          </div>
+        </div>
+        <div className="row current-conditions">
+          <div className="col-6 text-start">
+            <div>
+              <i className="bi bi-thermometer-high"></i>
+              <span id="temp-max">{Math.round(weatherData.tempMax)}</span>°C
+            </div>
+            <div>
+              <i className="bi bi-thermometer-low"></i>
+              <span id="temp-min">{Math.round(weatherData.tempMin)}</span>
+              °C
+            </div>
+          </div>
+          <div className="col-6 text-end">
+            <div>
+              Wind <i className="bi bi-wind"></i>
+            </div>
+            <div>
+              <span id="wind-speed">{weatherData.wind}</span> m/s
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div id="forecast"></div>
+        <hr />
+        <form id="change-city-form">
+          <div className="row">
+            <div className="col-6 align-self-center">
+              <input
+                type="search"
+                className="form-control search"
+                placeholder="City..."
+                autoComplete="off"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-6 align-self-center">
+              <button type="button" className="btn btn-outline-dark">
+                Search
+              </button>
+              <button type="button" className="btn btn-outline-dark">
+                Current city
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    const apiKey = "f96a36c366f556dae54ef30478f423d0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios(apiUrl).then(handleResponse);
+    return "Loading";
+  }
+}
