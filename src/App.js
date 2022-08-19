3b037459768d7e6d91e795649e642514835d96cf
hanna biloi <beloy1991@gmail.com>
2022-08-19 14:22:45 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Hanna Biloi and is{" "}
-        <a
-          href="httfooters://github.com/hannabiloi/weather-application-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Hanna Biloi and is{" "}
+          <a
+            href="httfooters://github.com/hannabiloi/weather-application-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
